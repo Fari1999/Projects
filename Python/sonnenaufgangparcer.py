26b0ae22ad6bf867aa5438fa9bb172dd7751227d
Fari1999 <faris.ichoutene@gmail.com>
2025-01-18 13:34:22 +0100

    Update sonnenaufgangparcer.py

--- Python/sonnenaufgangparcer.py
+++ Python/sonnenaufgangparcer.py
@@ -1,3 +1,4 @@
+#vorher pip install requests Beautifulsoup
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,7 +6,7 @@ URL = "https://sonnenaufgang.online/sun/velbert"
 
 response = requests.get(URL)
 
-soup = BeautifulSoup(response.text, "html.parser")
-sunrise_data = soup.find("li",{"data-name": "sunrise"})
-sunrise_data = sunrise_data.text
+soup = BeautifulSoup(response.text, "html.parser") #ist html
+sunrise_data = soup.find("li",{"data-name": "sunrise"}) 
+sunrise_data = sunrise_data.text #Gibt nur den Text aus
 print(sunrise_data)
